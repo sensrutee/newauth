9c71f802146743037b7fe727eccb26fe9cb5bc9a
sensrutee <105232403+sensrutee@users.noreply.github.com>
2023-07-23 17:19:48 +0530

    Update settings.py

--- newauth/settings.py
+++ newauth/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'newauth.wsgi.application'
 #first pip install psycopg2
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE':  'myrdshost.rds.amazonaws.com',
         'NAME': 'blockchain',
         'USER': 'postgres',
         'PASSWORD': 'srutee',
@@ -150,6 +150,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID '
+AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
+AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'AWS_S3_REGION_NAME'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
